83da4c8244c85e84259b1d8d9bbe025c328ebf3e
mohsen <mbp.moh3n@gmail.com>
2023-01-23 23:10:11 +0330

    sd

---
+++ docker/pma/pma_secure.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+/**
+ * This is needed for cookie based authentication to encrypt password in
+ * cookie. Needs to be 32 chars long.
+ */
+$cfg['blowfish_secret'] = '123';
+
+/**
+ * Servers configuration
+ */
+$i = 0;
+
+/**
+ * First server
+ */
+$i++;
+/* Authentication type */
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+/* Server parameters */
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['Servers'][$i]['AllowRoot'] = false;
